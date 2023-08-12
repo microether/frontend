8a60fb3fc2ef6a3af22ab9a7367bab68b63baf9b
timmpfe <69406058+timmpfe@users.noreply.github.com>
2023-08-12 22:23:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //
-// Place any custom JS here
+// Smart-Contract Link
 //
 if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
     const web3 = new Web3(window.ethereum || window.web3.currentProvider);
@@ -25,4 +25,46 @@ async function getPoolBalance() {
 async function depositToPool(amount) {
     const senderAddress = await requestAccount();
     await contract.methods.deposit(amount).send({ from: senderAddress });
+}
+
+async function applyForLoan(loanAmount, dueDate, repaymentMethod, loanType, numGuarantors) {
+    const accounts = await web3.eth.getAccounts();
+    const borrowerAddress = accounts[0];
+
+    await microEtherContract.methods.applyForLoan(loanAmount, dueDate, repaymentMethod, loanType, numGuarantors)
+    .send({ from: borrowerAddress })
+    .on("receipt", (receipt) => {
+        console.log("Loan applied successfully!", receipt);
+    })
+    .on("error", (error) => {
+        console.error("There was an error applying for the loan:", error);
+    });
+}
+
+async function repayLoan(loanId, amount) {
+    const accounts = await web3.eth.getAccounts();
+    const borrowerAddress = accounts[0];
+
+    await microEtherContract.methods.repayLoan(loanId)
+    .send({ from: borrowerAddress, value: web3.utils.toWei(amount, 'ether') })
+    .on("receipt", (receipt) => {
+        console.log("Loan repaid successfully!", receipt);
+    })
+    .on("error", (error) => {
+        console.error("There was an error repaying the loan:", error);
+    });
+}
+
+async function vouchForLoan(loanId) {
+    const accounts = await web3.eth.getAccounts();
+    const userAddress = accounts[0];
+
+    await microEtherContract.methods.vouchForLoan(loanId)
+    .send({ from: userAddress })
+    .on("receipt", (receipt) => {
+        console.log("Vouched for loan successfully!", receipt);
+    })
+    .on("error", (error) => {
+        console.error("There was an error vouching for the loan:", error);
+    });
 }
